2d3867f5d145c9558f7fb958568656018d7532ba
Kazem Danaie <kdanaie@hotmail.com>
2018-04-16 12:13:27 +0200

    evt with errors

---
+++ db/migrate/20180415101028_add_columns_to_users.rb
@@ -0,0 +1,8 @@
+class AddColumnsToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :email, :string
+    add_column :users, :created_at, :datetime
+    add_column :users, :updated_at, :datetime
+  end
+end
