d282776fadf4f8f80792ca52130da382391cada5
Kazem Danaie <kdanaie@hotmail.com>
2018-04-16 12:49:12 +0200

    Deleting files

--- db/schema.rb
+++ db/schema.rb
@@ -12,18 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20180406154538) do
 
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
